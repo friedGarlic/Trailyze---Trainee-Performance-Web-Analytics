899532e551500d42d5bedc317557c95ee3e4d1ab
remcarlmerza <remcarlmerza@gmail.com>
2023-12-10 21:41:19 +0800

    crashes when integration of model happens

--- ML_ASP/Controllers/DashboardController.cs
+++ ML_ASP/Controllers/DashboardController.cs
@@ -1,9 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.ML;
+using ML_net;
 
 namespace ML_ASP.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly MLContext _context;
+        private readonly PredictionEngine<Performance_DataSet, Performance_Prediction> _predictionEngine;
+
+        public DashboardController()
+        {
+            _context = new MLContext();
+
+            // load the trained model
+            var modelPath = "path/to/your/saved/model";
+            var trainedModel = _context.Model.Load(modelPath, out var modelSchema);
+
+            // prediction engine
+            _predictionEngine = _context.Model.CreatePredictionEngine<Performance_DataSet, Performance_Prediction>(trainedModel);
+        }
+
         public IActionResult Index()
         {
             return View();
