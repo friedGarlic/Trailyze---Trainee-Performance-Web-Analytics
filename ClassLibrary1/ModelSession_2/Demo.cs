fbc3913a2dd280f1e72f75a26ee8945855ad016c
remcarlmerza <remcarlmerza@gmail.com>
2023-12-11 20:08:04 +0800

    added upload feature, added a new model

---
+++ ClassLibrary1/ModelSession_2/Demo.cs
@@ -0,0 +1,75 @@
+﻿using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf;
+using Accord.MachineLearning;
+using Accord.MachineLearning.VectorMachines;
+using Accord.MachineLearning.VectorMachines.Learning;
+using Accord.Statistics.Kernels;
+using Microsoft.ML;
+using ML_net.ModelSession_1;
+
+namespace ML_net.ModelSession_2
+{
+    public class Demo
+    {
+        public static int CountSpacesInPdf(string filePath)
+        {
+            int totalNumberOfSpaces = 0;
+
+            using (PdfReader pdfReader = new PdfReader(filePath))
+            {
+                using (PdfDocument pdfDocument = new PdfDocument(pdfReader))
+                {
+                    for (int page_number = 1; page_number <= pdfDocument.GetNumberOfPages(); page_number++)
+                    {
+                        var strategy = new SimpleTextExtractionStrategy();
+                        string text = PdfTextExtractor.GetTextFromPage(pdfDocument.GetPage(page_number), strategy);
+
+                        int countedSpacesPerPage = text.Split(' ').Length - 1;
+                        totalNumberOfSpaces += countedSpacesPerPage;
+                    }
+                }
+            }
+
+            return totalNumberOfSpaces;
+        }
+
+        public static void Execute()
+        {
+            var context = new MLContext();
+
+            //load data
+            var trainData = context.Data.LoadFromTextFile<Performance_DataSet>("./ModelSession_2/report_grade.csv",
+                hasHeader: true, separatorChar: ',');
+
+            var testTrainSplit = context.Data.TrainTestSplit(trainData, testFraction: 0.1);
+
+            var pipeline = context.Transforms.Concatenate("Features", "Grade")
+                .Append(context.Regression.Trainers.LbfgsPoissonRegression());
+
+            var model = pipeline.Fit(testTrainSplit.TrainSet);
+
+            var predictions = model.Transform(testTrainSplit.TrainSet);
+
+            var metrics = context.Regression.Evaluate(predictions);
+
+            var newdata = new Performance_DataSet
+            {
+                CompletionTime = 30,
+                PerformanceScore = 86,
+                FeedbackScore = 3.2f,
+                EmployeeID = 1
+            };
+
+            //predict engine
+            var predictFunc = context.Model.CreatePredictionEngine<Performance_DataSet, Performance_Prediction>(model);
+
+            var prediction = predictFunc.Predict(newdata);
+
+            var modelPath = "ModelSession_1/PerformancePrediction.zip";
+
+            context.Model.Save(model, trainData.Schema, modelPath);
+        }
+
+    }
+}
--- ML_ASP/Controllers/DashboardController.cs
+++ ML_ASP/Controllers/DashboardController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.ML;
 using ML_net.ModelSession_1;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 
 namespace ML_ASP.Controllers
 {
@@ -8,9 +11,11 @@ namespace ML_ASP.Controllers
     {
         private readonly MLContext _context;
         private readonly PredictionEngine<Performance_DataSet, Performance_Prediction> _predictionEngine;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment Environment;
 
-        public DashboardController()
+        public DashboardController(Microsoft.AspNetCore.Hosting.IHostingEnvironment _environment)
         {
+            Environment = _environment;
             _context = new MLContext();
 
             // load the trained model
@@ -36,7 +41,7 @@ namespace ML_ASP.Controllers
             return View();
         }
 
-        [HttpPost]
+        /*[HttpPost]
         public IActionResult FileManagement(Performance_DataSet input)
         {
             // Use the loaded model to make predictions
@@ -46,6 +51,45 @@ namespace ML_ASP.Controllers
             ViewBag.Prediction = prediction.PerformancePrediciton_Score;
 
             return View(input);
+        }*/
+
+        [HttpPost]
+        public IActionResult FileManagement(List<IFormFile> postedFiles)
+        {
+            // The folder name where you want to store the uploads within your project
+            string uploadFolderName = "Uploads";
+
+            // Get the absolute path to the project directory
+            string projectPath = Path.Combine(this.Environment.ContentRootPath, "ML_ASP");
+
+            // Combine the project path with the folder where you want to store uploads
+            string path = Path.Combine(projectPath, uploadFolderName);
+
+            // Check if the folder exists, create it if not
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            List<string> uploadedFiles = new List<string>();
+
+            foreach (IFormFile postedFile in postedFiles)
+            {
+                string fileName = Path.GetFileName(postedFile.FileName);
+
+                // Combine the upload folder path with the file name
+                string filePath = Path.Combine(path, fileName);
+
+                using (FileStream stream = new FileStream(filePath, FileMode.Create))
+                {
+                    postedFile.CopyTo(stream);
+                    uploadedFiles.Add(fileName);
+                    ViewBag.Message += string.Format("<b>{0}</b> uploaded.<br />", fileName);
+                }
+            }
+
+            return View();
         }
+
     }
 }
