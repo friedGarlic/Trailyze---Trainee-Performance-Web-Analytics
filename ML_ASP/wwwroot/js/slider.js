fb17935376adcf7ca10ec4ba7a8adb34426f6ff2
remcarlmerza <remcarlmerza@gmail.com>
2023-12-18 13:14:49 +0800

    fix homepage

--- ML_ASP/wwwroot/js/slider.js
+++ ML_ASP/wwwroot/js/slider.js
@@ -1,10 +1,23 @@
-﻿
-document.getElementById('next').onclick = function() {
+﻿var counter = 1;
+setInterval(function () {
+    var radioElement = document.getElementById('radio' + counter);
+
+    if (radioElement) {
+        radioElement.checked = true;
+        counter++;
+    } else {
+        counter = 1;
+    }
+}, 4000);
+
+/*
+document.getElementById('next').onclick = function () {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slider').appendChild(lists[1]);
 }
 
-document.getElementById('prev').onclick = function(){
+document.getElementById('prev').onclick = function () {
     let lists = document.querySelectorAll('.item');
     document.getElementById('slider').prepend(lists[lists.length - 1]);
 }
+*/
