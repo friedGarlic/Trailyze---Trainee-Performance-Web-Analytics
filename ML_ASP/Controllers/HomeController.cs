9e413ad9a92b66b72c9d989300dc2a64ac2eb030
remcarlmerza <remcarlmerza@gmail.com>
2023-12-25 10:08:25 +0800

    fix so it works with laptop resolution

--- ML_ASP/Controllers/HomeController.cs
+++ ML_ASP/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace ML_ASP.Controllers
             return View();
         }
 
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
