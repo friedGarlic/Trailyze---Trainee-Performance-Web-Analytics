360a3a8c9ab179aa45eaeae5e4b513a28b1edfe4
remcarlmerza <remcarlmerza@gmail.com>
2024-01-17 10:17:01 +0800

    2fa permission and qrcode, microsoft only tho

--- ML_ASP.Utility/EmailSender.cs
+++ ML_ASP.Utility/EmailSender.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.UI.Services;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace ML_ASP.Utility
 {
@@ -11,6 +8,23 @@ namespace ML_ASP.Utility
     {
         public Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
+            var appPassword = "auzy btap dnhk yyea";
+
+            var emailToSend = new MimeMessage();
+            emailToSend.From.Add(MailboxAddress.Parse("TrailyzeFrom@test.com"));
+            emailToSend.To.Add(MailboxAddress.Parse(email));
+            emailToSend.Subject = subject;
+            emailToSend.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = htmlMessage };
+
+            //send email
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("smtp.gmail.com", 587, MailKit.Security.SecureSocketOptions.StartTls);
+                emailClient.Authenticate("remcarlmerza@gmail.com", appPassword);
+                emailClient.Send(emailToSend);
+                emailClient.Disconnect(true);
+            }
+
             return Task.CompletedTask;
         }
     }
