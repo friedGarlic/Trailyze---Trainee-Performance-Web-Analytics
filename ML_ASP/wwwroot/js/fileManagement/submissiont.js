c8ae2058f921d2dcab3e813c0d1ee9bf9404b7d9
remcarlmerza <remcarlmerza@gmail.com>
2024-02-03 12:56:15 +0800

    compile files that is uploaded into group

--- ML_ASP/wwwroot/js/fileManagement/submissiont.js
+++ ML_ASP/wwwroot/js/fileManagement/submissiont.js
@@ -2,24 +2,37 @@
 
 $(document).ready(function () {
     loadDataTable();
+
+    // Handle click event on the "Hide" button
+    $('#tblData').on('click', '.group-btn', function () {
+        var folderId = $(this).data('folderid');
+        toggleGroupVisibility(folderId);
+    });
 });
 
 function loadDataTable() {
     dataTable = $('#tblData').DataTable({
         "ajax": {
-            "url":"/FileManagement/GetAll"
+            "url": "/FileManagement/GetAll"
         },
         "columns": [
-            { "data": "name", "width": "15%"},
+            { "data": "name", "width": "15%" },
             { "data": "approvalStatus", "width": "15%" },
             { "data": "fileName", "width": "15%" }
         ],
         "rowGroup": {
             "dataSrc": "folderId",
             "startRender": function (rows, group) {
-                return '<button class="btn btn-primary btn-sm group-btn" data-folderid="' + group + '">Compiled</button> Folder ID: ' + group;
+                return '<button class="btn btn-primary btn-sm group-btn" data-folderid="' + group + '">Toggle</button> Folder ID: ' + group;
             }
         }
     });
 }
 
+function toggleGroupVisibility(folderId) {
+    var rows = dataTable.rows(function (idx, data, node) {
+        return data.folderId === folderId;
+    }).nodes();
+
+    $(rows).toggle(); // Toggle visibility of the rows with the specified folderId
+}
