cc4392ca3310157bb61a135112858dd3739d09f0
remcarlmerza <remcarlmerza@gmail.com>
2024-05-17 08:00:02 +0845

    added popup forms for requirements

--- ML_ASP/Program.cs
+++ ML_ASP/Program.cs
@@ -38,6 +38,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
---
+++ ML_ASP/wwwroot/js/Popup/CustomPopup.js
@@ -0,0 +1,28 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    var popup = document.getElementById('popup');
+
+    var addDocumentButton = document.getElementById('addDocumentButton');
+    var closeButton = document.querySelector('.close-btn');
+    var saveButton = document.getElementById('saveButton');
+
+    var addDocumentButton2 = document.getElementById('addDocumentButton2');
+    var addDocumentButton3 = document.getElementById('addDocumentButton3');
+
+    function showPopup() {
+        popup.style.display = 'flex';
+    }
+
+    function hidePopup() {
+        popup.style.display = 'none';
+    }
+
+    addDocumentButton2.addEventListener('click', showPopup);
+    addDocumentButton3.addEventListener('click', showPopup);
+
+    addDocumentButton.addEventListener('click', showPopup);
+    closeButton.addEventListener('click', hidePopup);
+    saveButton.addEventListener('click', function () {
+        // Add your save logic here
+        hidePopup();
+    });
+});
